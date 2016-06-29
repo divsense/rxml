574adfd9f3c78b89e058953d73303da9f5d6539e
oleg <oleg4564@me.com>
2016-06-29 00:43:25 -0400

    update

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var R = require("ramda");
 var State = require("momon").State;
 var parser = require("./rxml.js");
 
+var bodyL = R.lensProp('body');
+
+// noNils :: [a] -> [a]
+var noNils = R.reject(R.isNil);
+
+// tap :: x -> State(x, state)
+var of = R.curry((x,_) => State.of(x));
+
+var tail = ns => of(R.tail(ns));
+
 // isTextNode :: node -> Boolean
 var isTextNode = R.propEq('type','Text');
 	
@@ -18,27 +28,130 @@ var gtext = R.compose( R.map(R.prop('content')), R.filter( isTextNode ) );
 // globals :: [node] => State([node], state)
 var globals = ns => State.write(s => [R.filter( isntTextNode, ns ), R.over(R.lensProp('globals'), R.concat(gtext(ns)), s)]);
 
-// content :: [node] => State([node], state)
-var content = ns => {
-	
+// isDynamicAttr :: Obj -> Boolean
+isDynamicAttr = R.compose(R.test(/[{}]+/), R.prop('value'));
+
+// getAttr :: Obj -> [String]
+var getAttr = a => [ '"' + a.name + '"', '"' + a.value + '"' ];
+
+// staticAttrs :: [attr] -> [String]
+var staticAttrs = R.compose( R.flatten, R.map(getAttr), R.reject(isDynamicAttr) );
+
+// dynamicAttrs :: [attr] -> [String]
+var dynamicAttrs = R.compose( R.flatten, R.map(getAttr), R.filter(isDynamicAttr) );
+
+// stringify :: [String] -> String
+var stringify = R.compose( R.join(","), noNils, R.flatten );
+
+// defStatAttrs :: node -> String -> String
+var defStatAttrs = (node, uid) => {
+
+    var sa = staticAttrs( node.attributes );
+    if( sa.length ){
+        return 'var static_' + uid + ' = ' + '[' + staticAttrs( node.attributes ) + '];';
+    }
+
+}
+// addAttrs :: node -> String -> String
+var addAttrs = (node, uid) => {
+
+    var sa = staticAttrs( node.attributes );
+    var da = dynamicAttrs( node.attributes );
+
+    if( sa.length || da.length ){
+
+        var idd = sa.length && uid;
+
+        return [ 
+            idd ? '"' + idd + '"' : "null",
+            idd ? "static_" + idd : "null",
+            da.length ? da.join(",") : null
+        ];
+
+    }
+}
+
+// bodify :: String -> state -> state
+var bodify = (str,state) => R.compose( R.over(bodyL, R.append( str )) )( state );
+
+// elemify :: String -> node -> String -> state
+var elemify = (str, node, uid) => R.compose(
+      R.over( R.lensProp('uid'), R.inc)
+    , R.over( bodyL, R.append(str))
+    , R.over( R.lensProp('statics'), R.compose(noNils, R.append(defStatAttrs(node, uid))))
+    );
+
+// appendToBody :: String -> state -> state
+var appendToBody = R.curry((str,state) => [[],bodify(str,state)]);
+
+// textElement :: node -> State(_, state)
+var textElement = node => State.write(appendToBody("text(" + node.content + ");"));
+
+// voidElement :: node -> State(_, state)
+var voidElement = node => State.write( s => {
+
+    var uid = s.uid;
+
+    var buff = stringify(['elementVoid(' + node.name, addAttrs( node, uid )]) + ");";
+
+    return [node, elemify( buff, node, uid )( s )];
+});
+
+// compoundElement :: [node] -> State([node], state)
+var compoundElementBegin = node => State.write( s => {
+    var uid = s.uid;
+    var buff = stringify(['elementOpen(' + node.name, addAttrs( node, uid )]) + ")";
+    return [node, elemify( buff, node, uid )( s )];
+});
+
+var compoundElementEnd = node => State.write( s => {
+    return [node, R.over( bodyL, R.append( 'elementClose(' + node.name + ');' ), s)];
+});
+
+// goContent :: [node] => State([node], state)
+var goContent = ns => {
+
 	if( ns.length === 0 ){
-		return State.of("end");
+		return State.of([]);
 	}
-	
-	
-	State.write( s => {
-		var node = R.head( ns );
-		var s_ = R.over( R.lensProp('fs'), R.append( "function 1" ), s);
-		return [ R.tail( ns ), s_ ];
-});
 
-// addTemplate :: [node] => State([node], state)
-var addTemplate = ns => State.write( s => {
-		var node = R.head( ns );
-		var s_ = R.over( R.lensProp('fs'), R.append( "function 1" ), s);
-		return [ ns, s_ ];
-});
+	var node = ns[0];
 
+	if( node.type === 'Text' ){
+		return R.composeK( goContent, tail(ns), textElement )(State.of(node));
+	}
+	else if( node.type === 'SelfClosingTag' ){
+		return R.composeK( goContent, tail(ns), voidElement )(State.of(node));
+	}
+	else if( node.type === 'BalancedTag' ){
+		return R.composeK( 
+			   	goContent,
+				tail(ns),
+				compoundElementEnd,
+				of(node),
+			   	goContent,
+				of(node.content),
+			   	compoundElementBegin 
+				)(State.of(node));
+	}
+	else{
+		return State.of([]);
+	}
+}
+
+// defineFunc :: node -> String
+var defineFunc = node => {
+    var name = node.attributes.find( a => a.name === 'name' );
+    var args = "$id, $model, $context, $templates, $options";
+    return "export const " + name.value + " = ({" + args + "}) => {";
+}
+
+// beginTemplateFunc :: node => State(_, state)
+var beginTemplateFunc = node => State.write(appendToBody(defineFunc(node)));
+
+// endTemplateFunc :: node => State(_, state)
+//var endTemplateFunc = node => State.write(appendToBody( "}", R.tail(ns)) );
+var endTemplateFunc = node => State.write(appendToBody( "}"));
 
 // templates :: [node] => State([node], state)
 var templates = ns => {
@@ -46,9 +159,19 @@ var templates = ns => {
 		return State.of("end");
 	}
 
-	if( ns[0].name === 'template' ){
-		return R.composeK( templates, content, addTemplate)( State.of(ns) );
-	}
+	var node = ns[0];
+
+    if( node.name === 'template' ){
+        return R.composeK( 
+				templates,
+				tail(ns),
+				endTemplateFunc,
+				of(node),
+				goContent,
+				of(node.content),
+			   	beginTemplateFunc
+				)( State.of(node) );
+    }
 
 	return templates( R.tail( ns ) );
 }
@@ -58,10 +181,15 @@ var translate = R.composeK(templates, globals);
 var input = fs.readFileSync("./test.html", {encoding: 'utf8'});
 var model = parser.parse( input );
 
-var res = translate( State.of(model) ).run({globals:[], statics:[], fs:[]});
+console.log( model );
 
-console.log( res[1] );
+var res = translate( State.of(model) ).run({globals:[], statics:[], body:[],uid:1})[ 1 ];
 
+console.log( res );
+
+var buff = "\n\n" + res.globals.join("\n") + "\n" + res.statics.join("\n") + "\n" + res.body.join("\n") + "\n";
+
+console.log( buff );
 
 //module.exports = function(opts){
 //}
